a6308d78680814379b2ee6128e2e4b1c87e69175
JunseongKim0105 <juns.kim0105@gmail.com>
2023-03-08 17:14:58 -0500

    made new brunch

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,15 @@
 class ProductsController < ApplicationController
 
   def index
+    # uses MODEL
     @products = Product.all.order(created_at: :desc)
+    # VIEW
+    render :index
   end
 
   def show
     @product = Product.find params[:id]
+    @category
   end
 
 end
